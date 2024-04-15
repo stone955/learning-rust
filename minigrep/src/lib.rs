d5b3f9c11d8bc6be8df8fd77fe13082e1bdac35c
stone <335612970@qq.com>
2024-04-15 11:25:03 +0800

    使用迭代器改进程序

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -6,26 +6,6 @@ pub struct Config {
     pub case_sensitive: bool,
 }
 
-impl Config {
-    // 返回 Result 来替代直接 panic
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        
-        let case_sensitive = env::var("CASE_SENSITIVE").is_ok();
-
-        Ok(Config {
-            query,
-            file_path,
-            case_sensitive,
-        })
-    }
-}
-
 // impl Config {
 //     fn from(args: &[String]) -> Config {
 //         let query = args[1].clone();
@@ -41,6 +21,53 @@ impl Config {
 
 //     Config { query, file_path }
 // }
+
+// impl Config {
+//     // 返回 Result 来替代直接 panic
+//     pub fn build(args: &[String]) -> Result<Config, &'static str> {
+//         if args.len() < 3 {
+//             return Err("not enough arguments");
+//         }
+
+//         let query = args[1].clone();
+//         let file_path = args[2].clone();
+
+//         let case_sensitive = env::var("CASE_SENSITIVE").is_ok();
+
+//         Ok(Config {
+//             query,
+//             file_path,
+//             case_sensitive,
+//         })
+//     }
+// }
+
+impl Config {
+    // 返回 Result 来替代直接 panic
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        // 使用迭代器解析参数
+        // 第一个参数是程序名，由于无需使用，因此这里直接空调用一次
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get file path string"),
+        };
+
+        let case_sensitive = env::var("CASE_SENSITIVE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            case_sensitive,
+        })
+    }
+}
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // 读取文件
@@ -59,29 +86,32 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+// pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+//     let mut results = Vec::new();
+
+//     for line in contents.lines() {
+//         if line.contains(query) {
+//             results.push(line);
+//         }
+//     }
+
+//     results
+// }
+
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
 
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -2,7 +2,7 @@ use minigrep::{run, Config};
 use std::{env, process};
 
 fn main() {
-    // 迭代1:基本功能
+    // 迭代1: 基本功能
     // 问题：
     // - 单一且庞大的函数。
     //      对于 minigrep 程序而言， main 函数当前执行两个任务：解析命令行参数和读取文件。但随着代码的增加，main 函数承载的功能也将快速增加。
@@ -33,27 +33,45 @@ fn main() {
     //     println!("With text:\n{}", content);
     // }
 
-    // 迭代2:增加模块化和错误处理
-    {
-        // 读取命令行参数
-        let args: Vec<String> = env::args().collect();
+    // 迭代2: 增加模块化和错误处理
+    // {
+    //     // 读取命令行参数
+    //     let args: Vec<String> = env::args().collect();
 
-        // 分离命令行解析
-        // let config = parse_config(&args);
-        // 聚合配置变量
-        // let config = Config::from(&args);
-        // 处理返回的 Result
-        let config = Config::build(&args).unwrap_or_else(|err| {
+    //     // 分离命令行解析
+    //     // let config = parse_config(&args);
+    //     // 聚合配置变量
+    //     // let config = Config::from(&args);
+    //     // 处理返回的 Result
+    //     let config = Config::build(&args).unwrap_or_else(|err| {
+    //         // 重定向错误信息的输出
+    //         eprintln!("Problem parsing arguments: {}", err);
+    //         process::exit(1);
+    //     });
+
+    //     println!(
+    //         "Searching for {} from {}",
+    //         config.query,
+    //         config.file_path
+    //     );
+
+    //     // 分离主体逻辑, 处理返回的错误
+    //     if let Err(err) = run(config) {
+    //         // 重定向错误信息的输出
+    //         eprintln!("Application error: {}", err);
+    //         process::exit(1);
+    //     }
+    // }
+
+    // 迭代3: 使用迭代器改进程序
+    {
+        let config = Config::build(env::args()).unwrap_or_else(|err| {
             // 重定向错误信息的输出
             eprintln!("Problem parsing arguments: {}", err);
             process::exit(1);
         });
 
-        println!(
-            "Searching for {} from {}",
-            config.query,
-            config.file_path
-        );
+        println!("Searching for {} from {}", config.query, config.file_path);
 
         // 分离主体逻辑, 处理返回的错误
         if let Err(err) = run(config) {
